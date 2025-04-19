2ccf0693c96841cbd76fa62f23a35efe52966b08
Shivraj Soni <shivrajsoni09022005@gmail.com>
2025-04-20 02:43:31 +0530

    Added Meeting Setup

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,12 +1,93 @@
-import React from 'react'
+// 'use client';
 
-const Meeting = ({params}:{params:{id:string}}) => {
+// import MeetingRoom from '@/components/MeetingRoom';
+// import MeetingSetup from '@/components/MeetingSetup';
+// import { useUser } from '@clerk/nextjs'
+// import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
+// import React, { useState } from 'react'
+// import { UseGetCallById } from '../../../../../hooks/useGetCallById';
+// import Loader from '@/components/Loader';
+// import { useParams } from 'next/navigation';
+
+// const Meeting = ({params}:{params:{id:string}}) => {
+
+//   const {id} = useParams();
+//   const {user, isLoaded} = useUser();
+//   const [isSetupComplete, setisSetupComplete] = useState(false);
+//   const {call, isCallLoading} = UseGetCallById(id);
+  
+//   if(!isCallLoading || !isLoaded) return <Loader/>
+  
+//   if (!call) return (
+//     <p className="text-center text-3xl font-bold text-white">
+//       Call Not Found
+//     </p>
+//   );
+
+//   return (
+//     <main className='h-screen w-full'>
+//       <StreamCall call={call}>
+//         <StreamTheme>
+//           {!isSetupComplete?(
+//             <MeetingSetup setisSetupComplete={setisSetupComplete}/>
+//           ):(
+//             <MeetingRoom/>
+//           )}
+//         </StreamTheme>
+//       </StreamCall>
+
+//     </main>
+//   )
+// }
+
+// export default Meeting
+
+'use client';
+
+import { useState } from 'react';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation';
+import { Loader } from 'lucide-react';
+
+import { UseGetCallById } from '../../../../../hooks/useGetCallById';
+import Alert from '@/components/Alert';
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
+
+const MeetingPage = () => {
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
+  const { call, isCallLoading } = UseGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call) return (
+    <p className="text-center text-3xl font-bold text-white">
+      Call Not Found
+    </p>
+  );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+
   return (
-    <div>
-    Meeting Room 
-    {params.id}
-    </div>
-  )
-}
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
 
-export default Meeting
+        {!isSetupComplete ? (
+          <MeetingSetup setisSetupComplete={setIsSetupComplete} />
+        ) : (
+          <MeetingRoom />
+        )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
+
+export default MeetingPage;
